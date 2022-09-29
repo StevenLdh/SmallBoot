a1e34bf9a0a9774d869d20a2c40b2f213bd0b16b
李德华 <LDH.123>
2022-09-29 13:58:44 +0800

    read file

--- src/main/java/com/supper/smallboot/infrastructure/utils/DemoUnit.java
+++ src/main/java/com/supper/smallboot/infrastructure/utils/DemoUnit.java
@@ -4,6 +4,8 @@ import com.supper.smallboot.biz.vo.CustomerVO;
 import com.supper.smallboot.infrastructure.anaotation.ExcelCell;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,12 +24,35 @@ public class DemoUnit {
       * @date 2022-07-26 10:28
       */
      public static void main(String[] args) {
+         new DemoUnit().readResources("/settings/setting.txt");
+     }
+
+     /**
+      * 测试注解解析
+      * @author ldh
+      * @date 2022-09-29 11:43
+      */
+     private static  void readAnnotation(){
          List<CustomerVO.CustomerInfoVO> list = new ArrayList<>();
          list.add(new CustomerVO.CustomerInfoVO().setCustomerName("111").setCustomerNum("001").setCorpId(1L));
          Class<? extends CustomerVO.CustomerInfoVO> cls = list.get(0).getClass();
          Field[] fields= cls.getDeclaredFields();
          for (Field f: fields) {
              System.out.println(f.getAnnotation(ExcelCell.class).name());
+         }
+     }
+
+     /**
+      * 测试读取资源文件
+      * @author ldh
+      * @date 2022-09-29 11:44
+      */
+     private void readResources(String fileName){
+         try {
+             InputStream inputStream = this.getClass().getResourceAsStream(fileName);
+             FileUtils.getFileContent(inputStream);
+         } catch (IOException e) {
+             e.printStackTrace();
          }
      }
 }
--- src/main/java/com/supper/smallboot/infrastructure/utils/FileUtils.java
+++ src/main/java/com/supper/smallboot/infrastructure/utils/FileUtils.java
@@ -1,7 +1,33 @@
-package com.supper.smallboot.infrastructure.utils;/**
+package com.supper.smallboot.infrastructure.utils;
+
+import java.io.*;
+
+/**
  * @Author ldh
- * @Description 
+ * @Description
  * @Date 11:40 2022-09-29
  **/
 public class FileUtils {
+    /**
+     * 根据文件路径读取文件内容
+     *
+     * @param fileInPath
+     * @throws IOException
+     */
+    public static void getFileContent(Object fileInPath) throws IOException {
+        BufferedReader br = null;
+        if (fileInPath == null) {
+            return;
+        }
+        if (fileInPath instanceof String) {
+            br = new BufferedReader(new FileReader(new File((String) fileInPath)));
+        } else if (fileInPath instanceof InputStream) {
+            br = new BufferedReader(new InputStreamReader((InputStream) fileInPath));
+        }
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+    }
 }
